669ecd1037867b82e7b09770bfd2d2a81c802c27
Romario <romariokp72@gmail.com>
2020-05-30 15:34:25 -0500

    lunaris web agregada pestaña Bio

--- src/public/js/scroll.js
+++ src/public/js/scroll.js
@@ -1,4 +1,12 @@
-ScrollReveal().reveal('.showcase');
+
+//Home
+
 ScrollReveal().reveal('.container-text1', { delay:500 });
 ScrollReveal().reveal('.showcase2', { delay:500 });
 ScrollReveal().reveal('.social', { delay:500 });
+
+//BIO
+
+ScrollReveal().reveal('.text-bio', { delay:500 });
+ScrollReveal().reveal('.lunaris-logo', { delay:500 });
+ScrollReveal().reveal('.integrants', { delay:500 });
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,9 +3,19 @@ const {Router}  = require('express');
 const router = Router();
 
 router.get('/', (req, res) =>{
-    res.render('index');
+    res.render('index', { title: 'LUNÂRIS | Sitio Oficial'});
+});
+
+router.get('/bio', (req, res) =>{
+    res.render('bio', { title: 'LUNÂRIS | Bio'});
+});
+
+router.get('/gallery', (req, res) =>{
+    res.render('gallery', { title: 'LUNÂRIS | Gallery'});
+});
+
+router.get('/video', (req, res) =>{
+    res.render('video', { title: 'LUNÂRIS | Video'});
 })
-
-
 
 module.exports = router;
